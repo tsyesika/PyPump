43d0e78a86a6c39a2fa0ce1ad7849dec4cf43e7a
xray7224 <xray7224@googlemail.com>
2013-05-26 16:39:50 +0100

    Removes testing code

--- PyPump.py
+++ PyPump.py
@@ -350,9 +350,3 @@ class PyPump(object):
 
         return self.pump.open(request).read().decode("utf-8")
 
-if __name__ == "__main__":
-    pump = PyPump("Tsyesika@pump.megworld.co.uk", client_name="Muon", secure=True)
-    print(pump.get_registration())
-    print(pump.get_token())
-
-    pump.follow("stav@microca.st")
