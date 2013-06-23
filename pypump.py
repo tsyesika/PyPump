f3789f3aa3792c19bd33c3cbbdae1656f8cb5cc4
xray7224 <xray7224@googlemail.com>
2013-06-23 14:31:32 +0100

    Move to requests-oauthlib

--- pypump.py
+++ pypump.py
@@ -1,34 +1,36 @@
 # -*- coding: utf-8 -*-
+
 ##
-# Copyright (C) 2010-2012 Reality <tinmachin3@gmail.com> and Psychedelic Squid <psquid@psquid.net>
-# Copyright (C) 2013 Jessica T. (Tsyesika) <xray7224@googlemail.com>
+#   Copyright (C) 2010-2012 Reality <tinmachin3@gmail.com> and Psychedelic Squid <psquid@psquid.net>
+#   Copyright (C) 2013 Jessica T. (Tsyesika) <xray7224@googlemail.com>
 # 
-# This program is free software: you can redistribute it and/or modify 
-# it under the terms of the GNU General Public License as published by 
-# the Free Software Foundation, either version 3 of the License, or 
-# (at your option) any later version. 
+#   This program is free software: you can redistribute it and/or modify 
+#   it under the terms of the GNU General Public License as published by 
+#   the Free Software Foundation, either version 3 of the License, or 
+#   (at your option) any later version. 
 # 
-# This program is distributed in the hope that it will be useful, 
-# but WITHOUT ANY WARRANTY; without even the implied warranty of 
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
-# GNU General Public License for more details. 
+#   This program is distributed in the hope that it will be useful, 
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of 
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+#   GNU General Public License for more details. 
 # 
-# You should have received a copy of the GNU General Public License 
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#   You should have received a copy of the GNU General Public License 
+#   along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##
 
 import urllib.request
-import urllib.error
-import urllib.parse
 import json
 
-import oauth.oauth as oauth
+import requests
+from requests_oauthlib import OAuth1
+import openid
 import loader
 
 from exceptions import PyPumpException
 
 class PyPump(object):
 
+    PARAM_VERIFER = "oauth_verifier"
     PARAM_TOKEN = "oauth_token"
     PARAM_TOKEN_SECRET = "oauth_token_secret"
 
@@ -37,7 +39,7 @@ class PyPump(object):
     loader = None
 
     def __init__(self, server, key=None, secret=None, 
-                client_name="", client_type="web", token=None, 
+                client_name="", client_type="native", token=None, 
                 token_secret=None, save_token=None, secure = False):
         """ This will initate the pump.io library 
         == required ==
@@ -62,35 +64,39 @@ class PyPump(object):
             self.nickname = None # should be set with <instance>.set_nickname(<nickname>)
 
         if secure:
-            self.proto = "https://"
+            self.proto = "https"
         else:
-            self.proto = "http://"
+            self.proto = "http"
         
         self.loader = loader.Loader(self)
 
-        # oauthy stuff
-        if not (key and secret):
-            # okay we should assume then we're dynamically registrering a client
-            self.consumer = oauth.OAuthConsumer(
-                client_name=client_name, 
-                client_type=client_type,
-                server="{proto}{server}{endpoint}".format(proto=self.proto, server=self.server, endpoint=self.URL_CLIENT_REGISTRATION)
-            )
+        # first, if we need to register our client
+        if not (key or secret):
+            oid = openid.OpenID(
+                    protocol=secure,
+                    server=server,
+                    client_name=client_name,
+                    application_type=client_type
+                    )
+            self.consumer = oid.register_client()
         else:
-            self.consumer = oauth.OAuthConsumer(key, secret)
+            self.consumer = openid.Consumer()
+            self.consumer.key = key
+            self.consumer.secret = secret
 
-        
-        self.pump = urllib.request.build_opener()
-        
         if not (token and token_secret):
             # we need to make a new oauth request
             self.oauth_request() # this does NOT return access tokens but None
         else:
             self.token = token
             self.token_secret = token_secret
-        
-        self.oauth_token = oauth.OAuthToken(self.token, self.token_secret)
 
+        self.client = OAuth1(
+                client_key=self.consumer.key,
+                client_secret=self.consumer.secret,
+                resource_owner_key=self.token,
+                resource_owner_secret=self.token_secret
+                )        
 
     ##
     # getters to expose some data which might be useful
@@ -114,46 +120,46 @@ class PyPump(object):
     ## 
     # server request stuff
     ##
-    def request(self, endpoint, method="GET", data="", raw=False, attempts=10):
+    def request(self, endpoint, method="GET", data="", raw=False, params=None, attempts=10):
         """ This will make a request to <proto>//<self.server>/<endpoint> with oauth headers
         proto = self.proto (https or http)
         method = GET (default), POST or PUT
         attempts = this is how many times it'll try re-attempting
         """
+
+        params = {} if params is None else params
+
         if not (self.token and self.token_secret):
             # this shouldn't happen but just incase
             raise PyPumpException('Need to initiate oauth')
 
-        if "/" == endpoint[0]:
+        if endpoint.startswith("/"):
             endpoint = endpoint[1:] # remove inital / as we add it
 
-        if data and isinstance(data, dict):
+        if data and isinstance(data, (dict, list)):
             # we actually need to make it into a json object as that's what pump.io deals with.
             data = json.dumps(data)
-        
+
         if raw is False:
-            endpoint = "%s%s/%s" % (self.proto, self.server, endpoint)
+            endpoint = "{proto}://{server}/{endpoint}".format(
+                    proto=self.proto,
+                    server=self.server,
+                    endpoint=endpoint
+                    )
+        
 
-        oauth_request = oauth.OAuthRequest.from_consumer_and_token(self.consumer, token=self.oauth_token, 
-                                                                   http_method=method, http_url=endpoint)
+        for attempt in range(attempts):
+            if method == "POST":
+                request = requests.post(endpoint, auth=self.client, params=params, data=data)
+            elif method == "GET":
+                request = requests.get(endpoint, auth=self.client, params=params)
 
-        oauth_request.sign_request(oauth.OAuthSignatureMethod_HMAC_SHA1(), self.consumer, self.oauth_token)
-        attempts_done = 0
-
-        while attempts_done < attempts:
-            try:
-                if method in ["PUT", "POST"]:
-                    request = urllib.request.Request(endpoint, headers=oauth_request.to_header('OAuth'))
-
-                    request.add_header("Content-Type", "application/json")
-                    request.data = data.encode()
-                
-                else:
-                    request = urllib.request.Request(endpoint, headers=oauth_request.to_header('OAuth'))
-                
-                return json.loads(self.pump.open(request).read().decode("utf-8"))
-            except Exception:
-                attempts_done += 1
+            if request.status_code == 200:
+                # huray!
+                return json.loads(request.content.decode("utf-8"))
+            elif request.status_code in [400]:
+                # can't do much
+                raise PyPumpException("Recieved a 400 bad request error. This is likely due to an OAuth failure")
 
         return '' # failed :(
 
@@ -164,67 +170,68 @@ class PyPump(object):
     def oauth_request(self):
         """ Makes a oauth connection """
         # get tokens from server and make a dict of them.
-        tokens = {}
         server_token = self.request_token()
-        for elem in server_token.split("&"):
-            key, value = elem.split("=")
-            tokens[key] = value
-
+        
         # now we need the user to authorize me to use their pump.io account
-        code = self.get_access(tokens[self.PARAM_TOKEN])
-        access = self.request_access(tokens[self.PARAM_TOKEN], tokens[self.PARAM_TOKEN_SECRET], code) 
-        access = access.split("&")
-        for item in access:
-            # don't change the order >.< as they both start with oauth_token
-            if item.startswith("oauth_token_secret"):
-                self.token_secret = item[19:] # len("oauth_token_secret=") == 19
-            elif item.startswith("oauth_token"):
-                self.token = item[12:] # len("oauth_token") == 12
-
+        server_token[self.PARAM_VERIFER] = self.get_access(tokens[self.PARAM_TOKEN])
+        access = self.request_access(tokens[self.PARAM_TOKEN], tokens[self.PARAM_TOKEN_SECRET], tokens[self.PARAM_VERIFIER]) 
+    
     def get_access(self, token):
         """ this asks the user to let us use their account """
 
         print("To allow us to use your pump.io please follow the instructions at:")
-        print("%s%s/oauth/authorize?oauth_token=%s" % (self.proto, self.server, token))
-        code = input("It'll give you a verifier code, please enter it: ")
-        code = code.replace(" ", "") # lets make sure they didn't put any spaces in it.
+        print("{proto}://{server}/oauth/authorize?oauth_token={token}".format(
+                proto=self.proto,
+                server=self.server,
+                token=token
+                ))
+        
+        code = input("Verifier Code: ").lstrp(" ").rstrip(" ")
         return code
 
     def request_token(self):
         """ Gets a request token so that we can then ask the user for access to the accoutn """
-        oauth_request = oauth.OAuthRequest.from_consumer_and_token(self.consumer, 
-                                                                   callback="oob", 
-                                                                   http_method="POST", 
-                                                                   http_url="%s%s/oauth/request_token" % (
-                                                                            self.proto, self.server
-                                                                            )
-                                                                   )
+        client = OAuth1(
+                client_key=self.consumer.key,
+                client_secret=self.consumer.secret,
+                callback_uri="oob"
+                )
+        
+        req = requests.post(
+                "{proto}://{server}/oauth/request_token".format(
+                        proto=self.proto,
+                        server=self.server
+                        ),
+                auth=client
+                )
+        
+        data = urllib.request.parse_qs(r.content)
+        data = {
+            "oauth_token": data[self.PARAM_TOKEN][0],
+            "oauth_token_secret": data[self.PARAM_TOKEN_SECRET][0]
+        }
 
-        oauth_request.sign_request(oauth.OAuthSignatureMethod_HMAC_SHA1(), self.consumer, None)
-
-        request = urllib.request.Request("%s%s/oauth/request_token" % (self.proto, self.server), 
-                                                                       data=oauth_request.to_postdata().encode(), 
-                                                                       headers=oauth_request.to_header('OAuth')
-                                        )
-
-        return self.pump.open(request).read().decode("utf-8")
+        return data
 
     def request_access(self, token, token_secret, code):
         """ This is for when we've got the user's access value and we're asking the server for our access token """
-        request_token = oauth.OAuthToken(token, token_secret)
-        oauth_request = oauth.OAuthRequest.from_consumer_and_token(self.consumer, 
-                                                                   token=request_token, 
-                                                                   verifier=code,
-                                                                   callback="oob", 
-                                                                   http_method="POST", 
-                                                                   http_url="%s%s/oauth/access_token" % (
-                                                                        self.proto, self.server)
-                                                                   )
+        client = OAuth1(
+                client_key=self.consumer.key,
+                client_secret=self.consumer.secret,
+                resource_owner_key=token,   
+                resource_owner_secret=token_secret,
+                verifier=code
+                )
 
-        oauth_request.sign_request(oauth.OAuthSignatureMethod_HMAC_SHA1(), self.consumer, request_token)
-        request = urllib.request.Request("%s%s/oauth/access_token" % (self.proto, self.server), 
-                                         data=oauth_request.to_postdata().encode(), 
-                                         headers=oauth_request.to_header())
+        req = requests.post("{proto}://{server}/oauth/access_token".format(
+                        proto=self.proto,
+                        server=self.server,
+                        ),
+                auth=client)
+        
+        data = urllib.request.parse_qs(req.content)
 
-        return self.pump.open(request).read().decode("utf-8")
-
+        self.token = data[self.PARAM_TOKEN]
+        self.token_secret = data[self.PARAM_TOKEN_SECRET]
+        
+        
