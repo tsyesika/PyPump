8359ebef04e76576526fc67569aa7e90d08c0d0e
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2016-07-15 20:18:57 +0100

    Test coverage for PyPump.PyPump._requester

--- tests/pypump_test.py
+++ tests/pypump_test.py
@@ -6,6 +6,8 @@ try:
 except ImportError:
     import mock
 
+from requests import exceptions as request_excs
+
 from pypump import PyPump
 
 
@@ -17,6 +19,22 @@ class PyPumpTest(TestCase):
         client = mock.Mock()
         verifier = mock.Mock()
         requests_mock.post.return_value.text = "%s=thingy&%s=secretthingy" % (PyPump.PARAM_TOKEN, PyPump.PARAM_TOKEN_SECRET)
+        # re-add exceptions to mocked library
+        requests_mock.exceptions = request_excs
 
         pump = PyPump(client, verifier, store=store)
         self.assertEqual(pump.protocol, "https")
+
+        fnc_mock = mock.Mock()
+        fnc_mock.side_effect = request_excs.ConnectionError
+        with self.assertRaises(request_excs.ConnectionError):
+            pump._requester(fnc_mock, "")
+        self.assertEqual(len(fnc_mock.call_args_list), 1)
+        self.assertEqual(pump.protocol, "https")
+
+        fnc_mock.reset_mock()
+        pump.verify_requests = False
+        with self.assertRaises(request_excs.ConnectionError):
+            pump._requester(fnc_mock, "")
+        self.assertEqual(len(fnc_mock.call_args_list), 2)
+        self.assertEqual(pump.protocol, "http")
